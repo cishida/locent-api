617d2791902266e315c3eb59b6b7d63269ccf2f5
Gigster Engineer <support@gigster.com>
2016-01-04 02:50:43 +0100

    changed redirect url directly in mailer view

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,5 +1,4 @@
 DeviseTokenAuth.setup do |config|
   config.token_lifespan = 3.days
-  config.default_confirm_success_url = "http://locent.helloworld.ng"
-
+  config.default_confirm_success_url = "http://locent.helloworld.ng/#/dashboard"
 end
