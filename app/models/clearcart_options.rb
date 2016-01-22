74e2cf7d162b47686ccd78042657ce8ad243536a
Gigster Engineer <support@gigster.com>
2016-01-22 15:49:52 +0100

    new clear cart defaults

--- app/models/clearcart_options.rb
+++ app/models/clearcart_options.rb
@@ -11,8 +11,8 @@ class ClearcartOptions < ActiveRecord::Base
         opt_in_message: 'Text your confirmation code to complete opt-in to Clearcart or text NO to cancel.',
         opt_in_refusal_message: 'You have successfully cancelled opt-in to Clearcart.',
         welcome_message: 'Welcome to Clearcart. You will receive SMS notifications when there are deals on items in your cart.',
-        initial_cart_abandonment_message: 'The {ITEM} in your cart is now {DISCOUNT} off. Text PAY to buy it now!',
-        follow_up_message: '{ITEM} is still {DISCOUNT} off and is still in your cart. Reply with PAY to buy it now!',
+        initial_cart_abandonment_message: 'The {ITEM} in your cart is now {DISCOUNT} off. Text PAY to buy it now at {PRICE}!',
+        follow_up_message: '{ITEM} is still {DISCOUNT} off and is still in your cart. Reply with PAY to buy it now at {PRICE}!',
         confirmation_message: 'Your {PRICE} order for {ITEM} is on the way! Your order number is {ORDERNUMBER}',
         number_of_times_to_message: 3,
         time_interval_between_messages: 72
