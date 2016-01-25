7b285496c0811a275d17f5870ece35ecdb1b32d1
Gigster Engineer <support@gigster.com>
2016-01-25 12:26:25 +0100

    fixed email redirect

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,4 +1,4 @@
 DeviseTokenAuth.setup do |config|
   config.token_lifespan = 3.days
-  config.default_confirm_success_url = "http://locent.helloworld.ng/#/dashboard"
+  config.default_confirm_success_url = "http://locent.lanre.co/#/dashboard"
 end
