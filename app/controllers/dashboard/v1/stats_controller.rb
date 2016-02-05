20cad17495ca2378c2a88813484ee0900b0ac713
Gigster Engineer <support@gigster.com>
2016-02-05 05:02:29 +0100

    fixed queries for messages

--- app/controllers/dashboard/v1/stats_controller.rb
+++ app/controllers/dashboard/v1/stats_controller.rb
@@ -2,7 +2,11 @@ class Dashboard::V1::StatsController < DashboardController
   before_action :set_organization
 
   def keyword
-    Message.where(purpose_type: "Order", organization: @organization).select{ |message| message.purpose.feature == "keyword"}.count
+    @feature = Feature.find_by_name(params[:feature])
+    @subscription = Subscription.find_by_organization_id_and_feature_id(@organization.id, @feature.id)
+
+    messages_count = Message.where(purpose_type: "Order", organization: @organization).select{ |message| message.purpose.feature == "keyword"}.count
+    active_customers_count = OptIn.where()
 
 
   end
