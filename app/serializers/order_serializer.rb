c0b4e3df2c1a30ae27f9926e6b6d82724736ce5e
Gigster Engineer <support@gigster.com>
2016-02-07 08:54:43 +0100

    adds created at to orders serializer

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :uid, :description, :price, :status
+  attributes :uid, :description, :price, :status, :created_at
 
   has_one :customer
 end
