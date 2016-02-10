5b0d74c441ff6570e23399cf0fff9e9339a3309c
Gigster Engineer <support@gigster.com>
2016-02-10 23:35:31 +0100

    added documentation comments to superadmin controller actions

--- app/controllers/superadmin/v1/shortcode_applications_controller.rb
+++ app/controllers/superadmin/v1/shortcode_applications_controller.rb
@@ -1,11 +1,32 @@
+# @restful_api 1.0
+#
+# Superadmin::ShortcodeApplications
+#
 class Superadmin::V1::ShortcodeApplicationsController < SuperAdminController
   before_action :authenticate_super_admin
 
+  # @url /superadmin/shortcode_applications
+  # @action GET
+  #
+  # Gets paginated list of all shortcode application
+  #
+  # @response_field [Array<ShortcodeApplications>] applications All shortcode applications
+  #
   def index
     @applications = ShortcodeApplication.all
     paginate json: @applications
   end
 
+  # @url /superadmin/shortcode_applications/:id/update_status
+  # @action PUT
+  #
+  # Updates status of shortcode application
+  #
+  # @required [Integer] id The application id.
+  # @required [String] status The application status. It could be "pending", "approved" or "rejected".
+  #
+  # @response_field [ShortcodeApplication] application The updated application
+  #
   def update_status
     param! :status, String, required: true
 
@@ -17,6 +38,16 @@ class Superadmin::V1::ShortcodeApplicationsController < SuperAdminController
     end
   end
 
+  # @url /superadmin/shortcode_applications/:id/assign_shortcode_to_organization
+  # @action POST
+  #
+  # Assign a shortcode to an organization
+  #
+  # @required [Integer] id The organization id.
+  # @required [String] shortcode The shortcode.
+  #
+  # @response_field [Organization] organization The updated organization
+  #
   def assign_shortcode_to_organization
     param! :shortcode, String, required: true
 
