d76167377ddf50a1adf7acea2a2ae0dbceb6a349
Gigster Engineer <support@gigster.com>
2016-02-11 17:08:25 +0100

    adds route and controller for loader.io

---
+++ app/controllers/loader_controller.rb
@@ -0,0 +1,5 @@
+class LoaderController < ApplicationController
+  def index
+
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
